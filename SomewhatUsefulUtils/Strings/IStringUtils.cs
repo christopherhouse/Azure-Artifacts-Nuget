44ec769d15e38eb271a184dc6cb4b8a6099b7369
Chris House <chhouse@microsoft.com>
2022-10-28 11:15:48 -0500

    added string util stuff

--- SomewhatUsefulUtils/Strings/IStringUtils.cs
+++ SomewhatUsefulUtils/Strings/IStringUtils.cs
@@ -1,16 +1,10 @@
-﻿namespace SomewhatUsefulUtils.Strings;
+﻿using System.Globalization;
+
+namespace SomewhatUsefulUtils.Strings;
 
 public interface IStringUtils
 {
     string ReverseString(string input);
-}
 
-public class StringUtils : IStringUtils
-{
-    public string ReverseString(string input)
-    {
-        var charArray = input.ToCharArray();
-        Array.Reverse(charArray);
-        return new string(charArray);
-    }
+    string ToTitleCase(string input, CultureInfo culture);
 }
---
+++ SomewhatUsefulUtils/Strings/StringUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace SomewhatUsefulUtils.Strings;
+
+public class StringUtils : IStringUtils
+{
+    public string ReverseString(string input)
+    {
+        var charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    public string ToTitleCase(string input, CultureInfo culture)
+    {
+        return culture.TextInfo.ToTitleCase(input);
+    }
+}
