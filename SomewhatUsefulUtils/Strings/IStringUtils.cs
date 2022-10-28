e03b54bfd93ccd9081ce063c56f74574b5af56be
Chris House <chhouse@microsoft.com>
2022-10-28 11:12:03 -0500

    added some questionably useful util functions

---
+++ SomewhatUsefulUtils/Strings/IStringUtils.cs
@@ -0,0 +1,16 @@
+﻿namespace SomewhatUsefulUtils.Strings;
+
+public interface IStringUtils
+{
+    string ReverseString(string input);
+}
+
+public class StringUtils : IStringUtils
+{
+    public string ReverseString(string input)
+    {
+        var charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
